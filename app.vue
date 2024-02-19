86f5d435650499c1e4f64995905978073e22b151
jeden1102 <bum123@o2.pl>
2024-02-19 22:09:57 +0100

    master. Basic configuration - tailwind init + google fonts download.

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h2 class="text-4xl font-secondary">Test subtitle</h2>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: [100, 300, 400, 700, 900],
+          Roboto: true,
+          "Cormorant Garamond": [300, 400, 500, 700],
+        },
+      },
+    ],
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
