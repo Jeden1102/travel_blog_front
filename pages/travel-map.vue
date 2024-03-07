2558a8cca55679ac7bfc6f72a81c5c49b3a1f5dd
jeden1102 <bum123@o2.pl>
2024-03-07 22:18:14 +0100

    master. Travel map -base styles + marker added.

--- pages/travel-map.vue
+++ pages/travel-map.vue
@@ -3,10 +3,22 @@
         <AtomsSubheading>Checkout the destinations I've been</AtomsSubheading>
         <AtomsHeading>Travel map</AtomsHeading>
         <template v-if="journeys">
-            <GoogleMap class="mt-8" :api-key="config.public.googleApiKey" style="width: 100%; height: 500px"
-                :center="journeys.center" :zoom="6">
+            <GoogleMap :disable-default-ui="true" :styles="custom" class="mt-8" :api-key="config.public.googleApiKey"
+                style="width: 100%; height: 500px" :center="journeys.center" :zoom="6">
                 <MarkerCluster>
-                    <Marker v-for="(location, i) in journeys.locations" :options="{ position: location }" :key="i" />
+                    <Marker v-for="(location, i) in journeys.locations" :options="{ position: location }" :key="i">
+                        <InfoWindow class="bg-gray-600 p-4">
+                            <p class="text-lg">
+                                {{ location.title }}
+                            </p>
+                            <p>{{ location.location }}</p>
+                            <p>{{ location.date }}</p>
+                            <AtomsButton v-if="location.blogURI" custom-class="mt-2 text-sm" type="link"
+                                :uri="location.blogURI">read more
+                                <Icon class="text-2xl" name="material-symbols-light:arrow-right-alt-rounded" />
+                            </AtomsButton>
+                        </InfoWindow>
+                    </Marker>
                 </MarkerCluster>
             </GoogleMap>
         </template>
@@ -15,7 +27,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { GoogleMap, Marker, MarkerCluster } from 'vue3-google-map'
+import { GoogleMap, Marker, MarkerCluster, InfoWindow } from 'vue3-google-map'
 
 const config = useRuntimeConfig()
 const journeysData = await getJourneys();
@@ -24,9 +36,15 @@ const journeys = computed(() => {
     const locations = []
     if (journeysData) {
         journeysData.data.journeys.data.forEach(journey => {
+            const blogURI = journey.attributes.blog.data ? `/blog/${convertToSlug(journey.attributes.blog?.data.attributes.title)}?id=${journey.attributes.blog?.data.id}` : false;
+            console.log(journey.attributes.blog)
             locations.push({
                 lat: +journey.attributes.location.lat,
                 lng: +journey.attributes.location.lon,
+                title: journey.attributes.title,
+                blogURI,
+                location: journey.attributes.location.display_name,
+                date: journey.attributes.date
             })
         })
         return {
@@ -36,4 +54,55 @@ const journeys = computed(() => {
     }
     return false
 })
+
+const custom =
+    [
+        {
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "stylers": [
+                {
+                    "color": "#131314"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [
+                {
+                    "color": "#131313"
+                },
+                {
+                    "lightness": 7
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 25
+                }
+            ]
+        }
+    ]
+
 </script>
+
+<style>
+.gm-style .gm-style-iw-c {
+    padding: 0;
+}
+
+.gm-ui-hover-effect>span {
+    background: white;
+}
+</style>
